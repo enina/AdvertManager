d1e2a4a73c6fea370e4e8ec4e61b93fa88028aa2
Nina Edelstein <Nina.roshak@gmail.com>
2012-01-25 00:06:39 +0200

    cleanup

--- src/java/com/mne/advertmanager/model/Author.java
+++ src/java/com/mne/advertmanager/model/Author.java
@@ -7,17 +7,7 @@ package com.mne.advertmanager.model;
 import java.io.Serializable;
 import java.util.Collection;
 import java.util.HashSet;
-import javax.persistence.Basic;
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.NamedQueries;
-import javax.persistence.NamedQuery;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
